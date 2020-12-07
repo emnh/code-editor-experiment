038668d3f33ca8b434d5f78f99098929e8484bad
emnh <hvidevold@gmail.com>
2020-12-07 21:05:55 +0100

    1 file modified

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ for (let x = 0.0; x <= window.innerWidth; x += width) {
       ", " +
       b +
       ") : vec3(0.0))";
+    
     // const f2 =
     //   "((" + cond + ") ? vec3(" + nx + ", " + ny + ", 0.0) : vec3(0.0))";
 
